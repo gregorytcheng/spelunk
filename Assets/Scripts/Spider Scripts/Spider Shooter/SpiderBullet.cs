2409f2b7432635a9d23e5dc499de8f95a1c418f5
gregorytcheng <gregorytcheng@gmail.com>
2019-04-28 22:39:25 -0400

    test

---
+++ Assets/Scripts/Spider Scripts/Spider Shooter/SpiderBullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderBullet : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider2D target)
+    {
+        //if the bullet hits the player
+        if (target.tag == "Player")
+        {
+            Destroy(target.gameObject);
+            Destroy(gameObject);
+        }
+
+        //if the bullet hits the ground
+        if (target.tag == "Ground")
+        {
+           Destroy(gameObject); 
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Spider Scripts/Spider Shooter/SpiderShooter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderShooter : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject bullet;
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Player")
+        {
+            Destroy(target.gameObject);
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        //wait between 2 and 7 seconds
+        yield return new WaitForSeconds(Random.Range(2, 7));
+
+        Instantiate(bullet, transform.position, Quaternion.identity);
+
+        StartCoroutine(Attack());
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Attack());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
