1d3a91898faf46bcfe99dce2b5ddcfc4da21fad9
gregorytcheng <gregorytcheng@gmail.com>
2019-04-30 00:24:14 -0400

    Door and collectables

---
+++ Assets/Scripts/Collectables Scripts/DiamondScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Door.instance != null)
+        {
+            Door.instance.collectablesCount++;            
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Player")
+        {
+            Destroy(gameObject);
+            if (Door.instance != null)
+            {
+                Door.instance.DecrementCollectables();
+
+            }
+        }
+    }
+}
