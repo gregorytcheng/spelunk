c9d1ae3a082cb82d411956ed2593a4bb4b1fc479
gregorytcheng <gregorytcheng@gmail.com>
2019-05-02 02:17:48 -0400

    MEMES

--- Assets/Scripts/Game Controllers/LevelController.cs
+++ Assets/Scripts/Game Controllers/LevelController.cs
@@ -5,14 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        
-    }
-    
+
     public void PlayLevel1()
     {
         SceneManager.LoadScene("Level1");
+    }
+    
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    
+    public void PlayLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+    
+    public void PlayLevel4()
+    {
+        SceneManager.LoadScene("Level4");
     }
 
     public void BackToMenu()
