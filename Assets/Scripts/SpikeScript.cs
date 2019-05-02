80c067464f25d0481a7ca8f9e4eaf18de0c52213
gregorytcheng <gregorytcheng@gmail.com>
2019-05-02 01:39:14 -0400

    Spikes

---
+++ Assets/Scripts/SpikeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeScript : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Player")
+        {
+            Destroy(target.gameObject);
+            GameObject.Find("Gameplay Controller").GetComponent<GameplayController>().PlayerDied();
+        }
+    }
+    
+}
